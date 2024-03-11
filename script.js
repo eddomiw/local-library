0b735c9ce1c72a39ec77fee1fefde221f2598a21
Edgar Dominguez <eddomiw@gmail.com>
2024-03-11 01:42:44 -0700

    Update security information

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Set up dotenv to load environment variables
+require("dotenv").config();
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  "mongodb+srv://eddomiw:Project0@cluster0.a2sblue.mongodb.net/local_library?retryWrites=true&w=majority";
+
+// Fetch MongoDB connection string from environment variables
+const mongoDB = process.env.MONGODB_URI;
+
 main().catch((err) => console.log(err));
+
 async function main() {
   await mongoose.connect(mongoDB);
 }
